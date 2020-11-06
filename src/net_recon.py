e02a0eafeec594f59e1e92332ca32a70d32246e5
Hiwazup <Martin.Neville@mycit.ie>
2020-11-06 18:12:00 +0000

    Updating to use function returning function method

--- src/net_recon.py
+++ src/net_recon.py
@@ -3,11 +3,6 @@
 from scapy.all import *
 import concurrent.futures
 
-arpDict = {}
-icmp_replies = []
-INTERFACE_IP = ""
-BASE_IP = ""
-
 
 def main():
     interface = ""
@@ -15,84 +10,84 @@ def main():
     active = False
     number_of_arguments = len(sys.argv)
 
-    if number_of_arguments - 1 == 0:
+    if number_of_arguments - 1 == 0 or number_of_arguments > 4:
         help()
     else:
-        try:
-            opts, args = getopt.getopt(sys.argv[1:], "hi:pa", ["help", "iface=", "passive", "active"])
-            for opt, arg in opts:
-                if opt in ('-h', "--help"):
-                    help()
-                elif opt in ('-i', "--iface"):
-                    interface = arg
-                    if not valid_interface(interface):
-                        print(interface + " is not a known interface")
-                        sys.exit()
-                elif opt in ('-p', "--passive"):
-                    passive = True
-                elif opt in ('-a', "--active"):
-                    active = True
-
-            if interface:
-                if passive and active:
-                    print("Please specify only one mode")
-                elif passive:
-                    passive_scan(interface)
-                elif active:
-                    active_recon(interface)
-                else:
-                    print("Mode not specified")
+        for argument in sys.argv[1:]:
+            if argument == "-i" or argument == "--iface":
+                pass
+            elif argument == "-p" or argument == "--passive":
+                passive = True
+            elif argument == "-a" or argument == "--active":
+                active = True
             else:
-                print("Interface not specified")
-        except getopt.GetoptError as e:
-            print(e)
+                interface = argument
+                if not is_valid_interface(interface):
+                    print(argument + " is not a known interface")
+                    sys.exit()
+
+        if interface:
+            if passive and active:
+                print("Please specify only one mode")
+            elif passive:
+                passive_scan(interface)
+            elif active:
+                active_recon(interface)
+            else:
+                print("Mode not specified")
+        else:
+            print("Interface not specified")
 
 
 def passive_scan(interface):
-    sniff(prn=handle_arp_packet, iface=interface, filter="arp")
+    arp_dict = {}
+    sniff(prn=filter_packets(arp_dict), iface=interface, filter="arp")
 
 
 def active_recon(interface):
-    global INTERFACE_IP
-    INTERFACE_IP = get_if_addr(interface)
-    global BASE_IP
-    BASE_IP = INTERFACE_IP[0: (INTERFACE_IP.rfind('.') + 1)]
-    with concurrent.futures.ThreadPoolExecutor(max_workers=255) as executor:
-        executor.map(send_icmp_request, range(1, 255))
+    interface_ip = get_if_addr(interface)
+    base_ip = interface_ip[0: (interface_ip.rfind('.') + 1)]
+    replies_list = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=254) as executor:
+        executor.map(send(interface_ip, base_ip, replies_list), range(1, 255))
 
-    if not icmp_replies:
+    if not replies_list:
         print("No replies received")
     else:
-        print("Replies received from : " + str(icmp_replies)[1:-1])
+        print("Replies received from : " + str(replies_list)[1:-1])
 
 
-def send_icmp_request(ip):
-    reply = sr1(IP(src=INTERFACE_IP, dst=BASE_IP + str(ip), ttl=64) / ICMP(), timeout=1, verbose=0)
-    if reply is not None:
-        icmp_replies.append(reply.src)
+def send(interface_ip, base_ip, replies_list):
+    def send_icmp_request(ip):
+        destination = base_ip + str(ip)
+        reply = sr1(IP(src=interface_ip, dst=destination, ttl=64) / ICMP(), timeout=1, verbose=0)
+        if reply is not None:
+            replies_list.append(reply.src)
+
+    return send_icmp_request
 
 
-def valid_interface(interface):
+def is_valid_interface(interface):
     interfaces = get_if_list()
     return interface in interfaces
 
 
-def handle_arp_packet(pkt):
-    arp_packet = pkt[ARP]
-    if arp_packet.op == 2:
-        ip = arp_packet.psrc
-        mac = arp_packet.hwsrc
-        if ip in arpDict:
-            list_of_macs = arpDict[ip]
-            if mac.lower() not in list_of_macs:
-                arpDict[ip].append(mac.lower())
-                #  TODO: What does this mean ->
-                #   If an IP address has already been stored but a different MAC address is seen then the script should
-                #   also store this additional MAC address.
-        else:
-            arpDict[ip] = [mac]
+def filter_packets(arp_dict):
+    def arp_packets_handler(pkt):
+        arp_packet = pkt[ARP]
+        if arp_packet.op == 2:
+            ip = arp_packet.psrc
+            mac = arp_packet.hwsrc
+            if ip in arp_dict:
+                list_of_macs = arp_dict[ip]
+                if mac.lower() not in list_of_macs:
+                    arp_dict[ip].append(mac.lower())
+            else:
+                arp_dict[ip] = [mac]
 
-        return f"Source IP: {ip}\t Source MAC: {mac}"
+            return f"Source IP: {ip}\t Source MAC: {mac}"
+
+    return arp_packets_handler
 
 
 def help():
