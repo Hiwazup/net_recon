abe3064bb2beb6c727cd9829ee200ac82041b514
Hiwazup <martin.neville@mycit.ie>
2020-11-15 19:35:36 +0000

    Add reminder thread

--- src/net_recon.py
+++ src/net_recon.py
@@ -4,49 +4,68 @@ from scapy.all import *
 import concurrent.futures
 
 
-# Main function that handles the received command line arguments and verifies that they're allowed.
+# Main function that ensures enough command line arguments have been provided.
 def main():
+    number_of_arguments = len(sys.argv) - 1
+    if number_of_arguments == 0 or number_of_arguments > 3:
+        help()
+    else:
+        handle_arguments(sys.argv[1:])
+
+
+# Handles the command line arguments
+def handle_arguments(arguments):
     interface = ""
     passive = False
     active = False
-    number_of_arguments = len(sys.argv)
-
-    if number_of_arguments - 1 == 0 or number_of_arguments > 4:
-        help()
-    else:
-        for argument in sys.argv[1:]:
-            if argument == "-i" or argument == "--iface":
-                pass
-            elif argument == "-p" or argument == "--passive":
-                passive = True
-            elif argument == "-a" or argument == "--active":
-                active = True
-            else:
-                interface = argument
-                if not is_valid_interface(interface):
-                    print(argument + " is not a known interface")
-                    sys.exit()
-
-        if interface:
-            if passive and active:
-                print("Please specify only one mode")
-            elif passive:
-                passive_scan(interface)
-            elif active:
-                active_recon(interface)
-            else:
-                print("Mode not specified")
+    for argument in arguments:
+        if argument == "-i" or argument == "--iface":
+            pass
+        elif argument == "-p" or argument == "--passive":
+            passive = True
+        elif argument == "-a" or argument == "--active":
+            active = True
         else:
-            print("Interface not specified")
+            interface = argument
+            if not is_valid_interface(interface):
+                print(argument + " is not a known interface")
+                exit()
+
+    if interface:
+        if passive and active:
+            print("Please specify only one mode")
+        elif passive:
+            passive_scan(interface)
+        elif active:
+            active_recon(interface)
+        else:
+            print("Mode not specified")
+    else:
+        print("Interface not specified")
 
 
 # Scans for arp packets on the specified interface and sends each packet to filter_packets
 def passive_scan(interface):
     arp_dict = {}
+    setup_passive_scan_reminder_thread()
     sniff(prn=filter_packets(arp_dict), iface=interface, filter="arp")
 
 
-# Returns a reference to arp_packets_handler.
+# Sets up the reminder thread
+def setup_passive_scan_reminder_thread():
+    reminder_thread = threading.Thread(target=print_reminder, name="Passive Scan Reminder Thread")
+    reminder_thread.daemon = True
+    reminder_thread.start()
+
+
+# Prints a message every minute reminding the user how to terminate the scan
+def print_reminder():
+    while True:
+        print("**You can use Ctrl-C at any time to terminate passive scan**")
+        time.sleep(60)
+
+
+# Returns a reference to arp_packet_handler.
 def filter_packets(arp_dict):
     # Stores the details of an ARP Packet if it's opcode is 2. arp_dict contains all found IP MAC Address pairings. If
     # a packet comes in with the same IP as a stored IP but a different MAC Address then this new MAC Address is
@@ -64,7 +83,7 @@ def filter_packets(arp_dict):
                 else:
                     arp_dict[ip] = [mac]
 
-                return f"Source IP: {ip}\t Source MAC: {mac}"
+                print(ip, *arp_dict[ip], sep='\t')
 
     return arp_packet_handler
 
@@ -75,7 +94,7 @@ def active_recon(interface):
     interface_ip = get_if_addr(interface)
     base_ip = interface_ip[0: (interface_ip.rfind('.') + 1)]
     replies_list = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=254) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=64) as executor:
         executor.map(send(interface_ip, base_ip, replies_list), range(1, 255))
 
     if not replies_list:
@@ -107,7 +126,7 @@ def is_valid_interface(interface):
 # Prints out the commands that are allowed to be used in the application
 def help():
     print("-i or --iface for network interface name\n-p or --passive for passive mode\n-a or --active for active mode")
-    sys.exit()
+    exit()
 
 
 if __name__ == "__main__":
